f5c64f4d28cfa3ee18ace57e8a7eb65ce78106f4
Martin Breuss <breuss.martin@gmail.com>
2017-01-10 20:44:45 +0000

    verify heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def checkdeploy
-    render html: "get up and touch your toes! feels mice!"
+    render html: "get up and touch your toes, feels mice!"
   end
+  
 end
