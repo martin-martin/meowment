10bc909a174df30aeb68be0695c207f0144c7555
Martin Breuss <breuss.martin@gmail.com>
2017-01-11 22:11:26 +0000

    stashing for safety

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # returns a title for the pages, depending on the page
+  def full_title(page_title = '')
+    base_title = "Meowment"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
